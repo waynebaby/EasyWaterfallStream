770013ba00a21c779187f4e5f766f5bc73061399
waywa <waywa@WAYWAW530W10>
2016-04-03 12:16:17 +0800

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyWaterfallStream
+{
+    public class Class1
+    {
+    }
+}
